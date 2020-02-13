4f6a0f92d44c6d488daf4de3c25b7205e720a3e8
bintangmau <bintangmaulanahabib@gmail.com>
2020-02-13 21:05:46 +0700

    init

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port = process.env.PORT || 2001
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
 
+app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
 
 io.on('connection' , socket=>{
     return null
